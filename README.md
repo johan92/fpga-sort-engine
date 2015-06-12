b13b11b3bcf4022e3b4b67c8fe000fa4561743c3
johan92 <johan92@yandex.ru>
2015-06-12 18:26:35 +0300

    added info about Fmax in README

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 fpga-sort-engine
 ================
 
-Just some engines that sort unsigned data unsigned.
+Just some engines that sort unsigned data.
 Writing on Verilog (SystemVerilog).
 
 At input and output uses Avalon-ST (Streaming) interface.
 
-gnome\_sort\_engine\_wrapper - realization on Gnome Sort.
+### gnome\_sort\_engine\_wrapper 
+Realization on Gnome Sort.
 
-sort\_engine\_with\_merge - uses N engines in parallel, and merge tree at output ( to reduce proccessing time ).
+For AWIDTH = 10, DWIDTH = 32: <br>
+Altera Cyclone V 5CBA2F17C8: 136 MHz, 207 ALM
+
+### sort\_engine\_with\_merge 
+Uses N engines in parallel, and merge tree at output ( to reduce proccessing time ).
+
+For AWIDTH = 10, DWIDTH = 32, ENGINE\_CNT = 2: <br>
+Altera Cyclone V 5CBA2F17C8: 93 MHz, 428 ALM
--- quartus_proj/top.sv
+++ quartus_proj/top.sv
@@ -10,12 +10,13 @@ module top #(
   input               sop_i,
   input               eop_i,
   input               val_i,
-  output              busy_o,
+  output              ready_o,
 
   output [DWIDTH-1:0] data_o,
   output              sop_o,
   output              eop_o,
-  output              val_o
+  output              val_o,
+  input               ready_i
 
 );
 
@@ -27,13 +28,14 @@ sort_engine_if #(
   .DWIDTH ( DWIDTH ) 
 ) pkt_out ( clk_i );
 
+// trigger for Fmax testing
 always_ff @( posedge clk_i )
   begin
     pkt_in.data <= data_i; 
     pkt_in.sop  <= sop_i;
     pkt_in.eop  <= eop_i;
     pkt_in.val  <= val_i;
-    busy_o      <= pkt_in.busy;
+    ready_o     <= pkt_in.ready;
   end
 
 always_ff @( posedge clk_i )
@@ -41,9 +43,11 @@ always_ff @( posedge clk_i )
     data_o <= pkt_out.data;
     sop_o  <= pkt_out.sop; 
     eop_o  <= pkt_out.eop; 
-    val_o  <= pkt_out.val; 
+    val_o  <= pkt_out.val;
+    pkt_out.ready <= ready_i;
   end
 
+/*
 gnome_sort_engine_wrapper #( 
   .AWIDTH                                 ( AWIDTH            ),
   .DWIDTH                                 ( DWIDTH            )
@@ -53,6 +57,21 @@ gnome_sort_engine_wrapper #(
 
   .pkt_i                                  ( pkt_in            ),
   .pkt_o                                  ( pkt_out           )
+);
+*/
+
+sort_engine_with_merge #( 
+  .AWIDTH     ( AWIDTH ),
+  .DWIDTH     ( DWIDTH ),
+
+  .ENGINE_CNT ( 2      )
+) sewm (
+  .clk_i                                  ( clk_i             ),
+  .rst_i                                  ( rst_i             ),
+
+  .pkt_i                                  ( pkt_in            ),
+  .pkt_o                                  ( pkt_out           )
+
 );
 
 endmodule
